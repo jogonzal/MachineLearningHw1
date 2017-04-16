da20161cb0e59c45dc4d3356528500446f36c35b
Jorge <jogonzal@microsoft.com>
2017-04-16 14:40:01 -0700

    Add a test for an attribute value you've never seen

--- MachineLearningHw1UnitTests/DecisionTreeUnitTests.cs
+++ MachineLearningHw1UnitTests/DecisionTreeUnitTests.cs
@@ -23,7 +23,6 @@ namespace MachineLearningHw1UnitTests
 		{
 			VerifyDecisionTreeCanLearnTable(new List<DataSetValue>()
 			{
-				// x0
 				new DataSetValue(new List<string>() {"0", "0", "0"}, false),
 				new DataSetValue(new List<string>() {"0", "0", "1"}, false),
 				new DataSetValue(new List<string>() {"0", "1", "0"}, false),
@@ -40,7 +39,6 @@ namespace MachineLearningHw1UnitTests
 		{
 			VerifyDecisionTreeCanLearnTable(new List<DataSetValue>()
 			{
-				// x0
 				new DataSetValue(new List<string>() {"0", "0", "0"}, false),
 				new DataSetValue(new List<string>() {"0", "0", "1"}, false),
 				new DataSetValue(new List<string>() {"0", "1", "0"}, false),
@@ -57,7 +55,6 @@ namespace MachineLearningHw1UnitTests
 		{
 			VerifyDecisionTreeCanLearnTable(new List<DataSetValue>()
 			{
-				// x0
 				new DataSetValue(new List<string>() {"0", "0", "0"}, false),
 				new DataSetValue(new List<string>() {"0", "0", "1"}, false),
 				new DataSetValue(new List<string>() {"0", "1", "0"}, false),
@@ -74,7 +71,6 @@ namespace MachineLearningHw1UnitTests
 		{
 			VerifyDecisionTreeCanLearnTable(new List<DataSetValue>()
 			{
-				// x0
 				new DataSetValue(new List<string>() {"0", "0", "0"}, true),
 				new DataSetValue(new List<string>() {"0", "0", "1"}, true),
 				new DataSetValue(new List<string>() {"0", "1", "0"}, true),
@@ -91,7 +87,6 @@ namespace MachineLearningHw1UnitTests
 		{
 			VerifyDecisionTreeCanLearnTable(new List<DataSetValue>()
 			{
-				// x0
 				new DataSetValue(new List<string>() {"0", "0", "0"}, false),
 				new DataSetValue(new List<string>() {"0", "0", "1"}, false),
 				new DataSetValue(new List<string>() {"0", "1", "0"}, false),
@@ -109,7 +104,6 @@ namespace MachineLearningHw1UnitTests
 		{
 			VerifyDecisionTreeCanLearnTable(new List<DataSetValue>()
 			{
-				// x0
 				new DataSetValue(new List<string>() {"0", "0", "0"}, false),
 				new DataSetValue(new List<string>() {"0", "0", "1"}, true),
 				new DataSetValue(new List<string>() {"0", "1", "0"}, true),
@@ -126,7 +120,6 @@ namespace MachineLearningHw1UnitTests
 		{
 			VerifyDecisionTreeCanLearnTable(new List<DataSetValue>()
 			{
-				// x0
 				new DataSetValue(new List<string>() {"0", "0", "0"}, false),
 				new DataSetValue(new List<string>() {"0", "0", "1"}, true),
 				new DataSetValue(new List<string>() {"0", "1", "0"}, true),
@@ -136,6 +129,28 @@ namespace MachineLearningHw1UnitTests
 				new DataSetValue(new List<string>() {"1", "1", "0"}, false),
 				new DataSetValue(new List<string>() {"1", "1", "1"}, true),
 			});
+		}
+
+		[TestMethod]
+		public void TestWithDataYouveNeverSeen_StillWorks()
+		{
+			DecisionTreeLevel decisionTree = new DecisionTreeLevel(_generic3BooleanAttributes,
+			new List<DataSetValue>()
+			{
+				new DataSetValue(new List<string>() {"0", "0", "0"}, false),
+				new DataSetValue(new List<string>() {"0", "0", "1"}, true),
+				new DataSetValue(new List<string>() {"0", "1", "0"}, false),
+				new DataSetValue(new List<string>() {"0", "1", "1"}, false),
+				new DataSetValue(new List<string>() {"1", "0", "0"}, false),
+				new DataSetValue(new List<string>() {"1", "0", "1"}, false),
+				new DataSetValue(new List<string>() {"1", "1", "0"}, false),
+				new DataSetValue(new List<string>() {"1", "1", "1"}, false),
+			},
+			0);
+
+			decisionTree.D3();
+
+			decisionTree.Evaluate(new List<string>() { "0", "0", "JORGE" }).Should().BeFalse();
 		}
 
 		private void VerifyDecisionTreeCanLearnTable(List<DataSetValue> tableToLearn)
